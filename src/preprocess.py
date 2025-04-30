2ec1b84936f94372dcb161e374fb2d13d138c637
Luai Waleed <mistaluai@gmail.com>
2025-05-01 01:03:47 +0300

    fixed the hand written box extractor

--- src/preprocess.py
+++ src/preprocess.py
@@ -150,17 +150,17 @@ class HandwrittenExtractor:
         plt.axis('off')
 
         plt.subplot(2, 3, 3)
-        plt.imshow(p["binary_mask"], cmap='gray')
+        plt.imshow(p["binary_mask"], cmap='blue_image')
         plt.title(f'Mask ')
         plt.axis('off')
 
         plt.subplot(2, 3, 4)
-        plt.imshow(p["blue_emphasized"], cmap='gray')
+        plt.imshow(p["blue_emphasized"], cmap='blue_image')
         plt.title('Blue-Emphasized')
         plt.axis('off')
 
         plt.subplot(2, 3, 5)
-        plt.imshow(p["clean_mask"], cmap='gray')
+        plt.imshow(p["clean_mask"], cmap='blue_image')
         plt.title(f'Mask ')
         plt.axis('off')
 
@@ -178,15 +178,113 @@ class HandwrittenExtractor:
     def get_cropped_image(self):
         return self.cropped
 
+    def get_blue_cropped_image(self):
+        return self._emphasize_blue(self.cropped)
+
     def get_clean_mask(self):
         return self.mask
 
     def get_visualized_result(self):
         return self.result_img
 
+
+class HandwrittenBoxExtractor:
+    def __init__(self, image, blue_image):
+        self.original_image = image
+        self.blue_image = blue_image
+        self.binary = None
+        self.dilated = None
+        self.final_boxes = []
+        self.handwritten_boxes = []
+
+    def preprocess(self, visualize):
+        # Step 1: Binary inverse thresholding
+        _, self.binary = cv2.threshold(self.blue_image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+        self.binary = cv2.bitwise_not(self.binary)
+        if visualize:
+            self._show_image(self.binary, "Binary Thresholded (Otsu Inverse)", cmap='gray')
+
+        # Step 2: Morphological Opening to remove noise
+        kernel_open = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+        opened = cv2.morphologyEx(self.binary, cv2.MORPH_OPEN, kernel_open, iterations=1)
+        if visualize:
+            self._show_image(opened, "After Morphological Opening", cmap='gray')
+
+        # Step 3: Dilation to group characters
+        kernel_dilate = cv2.getStructuringElement(cv2.MORPH_RECT, (30, 10))
+        self.dilated = cv2.dilate(opened, kernel_dilate, iterations=3)
+        if visualize:
+            self._show_image(self.dilated, "After Dilation", cmap='gray')
+
+    def find_and_filter_boxes(self, visualize):
+        contours, _ = cv2.findContours(self.dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        boxes = [cv2.boundingRect(c) for c in contours if cv2.contourArea(c) > 300]
+        self.final_boxes = self.non_max_suppression_fast(boxes)
+        if visualize:
+            self.visualize_boxes(self.final_boxes, title="Refined Word Bounding Boxes", color=(0, 255, 0))
+
+    def non_max_suppression_fast(self, boxes, overlapThresh=0.3):
+        if len(boxes) == 0:
+            return []
+
+        boxes = np.array(boxes)
+        pick = []
+
+        x1 = boxes[:, 0]
+        y1 = boxes[:, 1]
+        x2 = boxes[:, 0] + boxes[:, 2]
+        y2 = boxes[:, 1] + boxes[:, 3]
+
+        area = boxes[:, 2] * boxes[:, 3]
+        idxs = np.argsort(y2)
+
+        while len(idxs) > 0:
+            last = idxs[-1]
+            pick.append(last)
+
+            xx1 = np.maximum(x1[last], x1[idxs[:-1]])
+            yy1 = np.maximum(y1[last], y1[idxs[:-1]])
+            xx2 = np.minimum(x2[last], x2[idxs[:-1]])
+            yy2 = np.minimum(y2[last], y2[idxs[:-1]])
+
+            w = np.maximum(0, xx2 - xx1)
+            h = np.maximum(0, yy2 - yy1)
+
+            overlap = (w * h) / area[idxs[:-1]]
+            idxs = np.delete(idxs, np.concatenate(([len(idxs) - 1], np.where(overlap > overlapThresh)[0])))
+
+        return boxes[pick].astype("int")
+
+    def visualize_boxes(self, boxes, title="Bounding Boxes", color=(0, 255, 0)):
+        img_copy = self.original_image.copy()
+        for (x, y, w, h) in boxes:
+            cv2.rectangle(img_copy, (x, y), (x + w, y + h), color, 2)
+
+        self._show_image(img_copy, title)
+
+    def _show_image(self, img, title, cmap=None):
+        plt.figure(figsize=(10, 6))
+        if len(img.shape) == 2:
+            plt.imshow(img, cmap=cmap if cmap else 'gray')
+        else:
+            plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        plt.title(title)
+        plt.axis("off")
+        plt.show()
+
+    def run(self, visualize=False):
+        self.preprocess(visualize)
+        self.find_and_filter_boxes(visualize)
+
 if __name__ == "__main__":
     # path = '/Users/mistaluai/Documents/Github Repos/Prescriptions-OCR/data/test/Beauty_prescription_1.jpg'
     # path = '/Users/mistaluai/Documents/Github Repos/Prescriptions-OCR/data/test/prescription_opth_203.jpg'
     path = '/Users/mistaluai/Documents/Github Repos/Prescriptions-OCR/data/test/prescription_opth_183.jpg'
     extractor = HandwrittenExtractor(path)
-    extractor.show_debug_plot()
+    # extractor.show_debug_plot()
+
+    image = extractor.get_cropped_image()
+    blue_image = extractor.get_blue_cropped_image()
+
+    box_extractor = HandwrittenBoxExtractor(image, blue_image)
+    box_extractor.run(visualize=False)
